d58a448c57b8936a10eaa94590e55b690d97d01b
Sven Reissmann <sven@0x80.io>
2012-07-09 17:28:14 +0200

    modified db layout

--- createdb.sql
+++ createdb.sql
@@ -4,7 +4,7 @@
 
 -- Dumped from database version 9.1.4
 -- Dumped by pg_dump version 9.1.4
--- Started on 2012-07-07 01:02:22 CEST
+-- Started on 2012-07-09 17:23:46 CEST
 
 SET statement_timeout = 0;
 SET client_encoding = 'UTF8';
@@ -21,7 +21,7 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 
 
 --
--- TOC entry 1882 (class 0 OID 0)
+-- TOC entry 1883 (class 0 OID 0)
 -- Dependencies: 163
 -- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: 
 --
@@ -37,7 +37,7 @@ SET default_with_oids = false;
 
 --
 -- TOC entry 161 (class 1259 OID 16923)
--- Dependencies: 1862 1863 1864 1865 1866 1867 1868 6
+-- Dependencies: 1862 1863 1865 1866 1867 1868 1869 1430 6 1430 1430 1430 1430
 -- Name: events; Type: TABLE; Schema: public; Owner: syslog; Tablespace: 
 --
 
@@ -45,21 +45,103 @@ CREATE TABLE events (
     id integer NOT NULL,
     received_time timestamp without time zone DEFAULT now(),
     reported_time timestamp without time zone,
-    host character varying(64) DEFAULT NULL::character varying,
-    facility character varying(10) DEFAULT NULL::character varying,
-    priority character varying(10) DEFAULT NULL::character varying,
+    host character varying(256) COLLATE pg_catalog."en_US.utf8" DEFAULT NULL::character varying,
+    facility character varying(8) DEFAULT NULL::character varying,
+    severity character varying(16) COLLATE pg_catalog."en_US.utf8" DEFAULT NULL::character varying,
     level character varying(10) DEFAULT NULL::character varying,
-    tag character varying(10) DEFAULT NULL::character varying,
-    program character varying(15) DEFAULT NULL::character varying,
-    message text
+    tag character varying(2) DEFAULT NULL::character varying,
+    program character varying(32) COLLATE pg_catalog."en_US.utf8" DEFAULT NULL::character varying,
+    message text COLLATE pg_catalog."en_US.utf8",
+    fqdn character varying(256) COLLATE pg_catalog."en_US.utf8"
 );
 
 
 ALTER TABLE public.events OWNER TO syslog;
 
 --
+-- TOC entry 1884 (class 0 OID 0)
+-- Dependencies: 161
+-- Name: COLUMN events.received_time; Type: COMMENT; Schema: public; Owner: syslog
+--
+
+COMMENT ON COLUMN events.received_time IS 'Timestamp the log event was added to the database';
+
+
+--
+-- TOC entry 1885 (class 0 OID 0)
+-- Dependencies: 161
+-- Name: COLUMN events.reported_time; Type: COMMENT; Schema: public; Owner: syslog
+--
+
+COMMENT ON COLUMN events.reported_time IS 'Timestamp the log event was generated';
+
+
+--
+-- TOC entry 1886 (class 0 OID 0)
+-- Dependencies: 161
+-- Name: COLUMN events.host; Type: COMMENT; Schema: public; Owner: syslog
+--
+
+COMMENT ON COLUMN events.host IS 'IP address or DNS name of the logging host';
+
+
+--
+-- TOC entry 1887 (class 0 OID 0)
+-- Dependencies: 161
+-- Name: COLUMN events.facility; Type: COMMENT; Schema: public; Owner: syslog
+--
+
+COMMENT ON COLUMN events.facility IS 'The facility that generated the log event';
+
+
+--
+-- TOC entry 1888 (class 0 OID 0)
+-- Dependencies: 161
+-- Name: COLUMN events.severity; Type: COMMENT; Schema: public; Owner: syslog
+--
+
+COMMENT ON COLUMN events.severity IS 'The priority/severity of the log event';
+
+
+--
+-- TOC entry 1889 (class 0 OID 0)
+-- Dependencies: 161
+-- Name: COLUMN events.tag; Type: COMMENT; Schema: public; Owner: syslog
+--
+
+COMMENT ON COLUMN events.tag IS 'Two-character TAG of the log event describing the facility and the priority';
+
+
+--
+-- TOC entry 1890 (class 0 OID 0)
+-- Dependencies: 161
+-- Name: COLUMN events.program; Type: COMMENT; Schema: public; Owner: syslog
+--
+
+COMMENT ON COLUMN events.program IS 'The program that generated the log event';
+
+
+--
+-- TOC entry 1891 (class 0 OID 0)
+-- Dependencies: 161
+-- Name: COLUMN events.message; Type: COMMENT; Schema: public; Owner: syslog
+--
+
+COMMENT ON COLUMN events.message IS 'The actual log message';
+
+
+--
+-- TOC entry 1892 (class 0 OID 0)
+-- Dependencies: 161
+-- Name: COLUMN events.fqdn; Type: COMMENT; Schema: public; Owner: syslog
+--
+
+COMMENT ON COLUMN events.fqdn IS 'Full qualified domain name of the logging host';
+
+
+--
 -- TOC entry 162 (class 1259 OID 16936)
--- Dependencies: 161 6
+-- Dependencies: 6 161
 -- Name: logs_id_seq; Type: SEQUENCE; Schema: public; Owner: syslog
 --
 
@@ -74,7 +156,7 @@ CREATE SEQUENCE logs_id_seq
 ALTER TABLE public.logs_id_seq OWNER TO syslog;
 
 --
--- TOC entry 1883 (class 0 OID 0)
+-- TOC entry 1893 (class 0 OID 0)
 -- Dependencies: 162
 -- Name: logs_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: syslog
 --
@@ -83,7 +165,16 @@ ALTER SEQUENCE logs_id_seq OWNED BY events.id;
 
 
 --
--- TOC entry 1869 (class 2604 OID 16938)
+-- TOC entry 1894 (class 0 OID 0)
+-- Dependencies: 162
+-- Name: logs_id_seq; Type: SEQUENCE SET; Schema: public; Owner: syslog
+--
+
+SELECT pg_catalog.setval('logs_id_seq', 1, true);
+
+
+--
+-- TOC entry 1864 (class 2604 OID 16938)
 -- Dependencies: 162 161
 -- Name: id; Type: DEFAULT; Schema: public; Owner: syslog
 --
@@ -102,7 +193,7 @@ ALTER TABLE ONLY events
 
 
 --
--- TOC entry 1870 (class 1259 OID 16961)
+-- TOC entry 1870 (class 1259 OID 17041)
 -- Dependencies: 161
 -- Name: events_idx_facility; Type: INDEX; Schema: public; Owner: syslog; Tablespace: 
 --
@@ -111,8 +202,8 @@ CREATE INDEX events_idx_facility ON events USING btree (facility);
 
 
 --
--- TOC entry 1871 (class 1259 OID 16960)
--- Dependencies: 161
+-- TOC entry 1871 (class 1259 OID 16971)
+-- Dependencies: 1430 161
 -- Name: events_idx_host; Type: INDEX; Schema: public; Owner: syslog; Tablespace: 
 --
 
@@ -120,17 +211,17 @@ CREATE INDEX events_idx_host ON events USING btree (host);
 
 
 --
--- TOC entry 1872 (class 1259 OID 16962)
--- Dependencies: 161
+-- TOC entry 1872 (class 1259 OID 16985)
+-- Dependencies: 1430 161
 -- Name: events_idx_priority; Type: INDEX; Schema: public; Owner: syslog; Tablespace: 
 --
 
-CREATE INDEX events_idx_priority ON events USING btree (priority);
+CREATE INDEX events_idx_priority ON events USING btree (severity);
 
 
 --
--- TOC entry 1873 (class 1259 OID 16963)
--- Dependencies: 161
+-- TOC entry 1873 (class 1259 OID 17012)
+-- Dependencies: 1430 161
 -- Name: events_idx_program; Type: INDEX; Schema: public; Owner: syslog; Tablespace: 
 --
 
@@ -147,7 +238,7 @@ CREATE INDEX events_idx_reported_time ON events USING btree (reported_time);
 
 
 --
--- TOC entry 1881 (class 0 OID 0)
+-- TOC entry 1882 (class 0 OID 0)
 -- Dependencies: 6
 -- Name: public; Type: ACL; Schema: -; Owner: postgres
 --
@@ -158,7 +249,7 @@ GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
 
 
--- Completed on 2012-07-07 01:02:22 CEST
+-- Completed on 2012-07-09 17:23:46 CEST
 
 --
 -- PostgreSQL database dump complete

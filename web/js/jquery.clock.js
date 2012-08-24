0fc2923b9539299a4554e0c42fe1430756d40500
Sven Reissmann <sven@0x80.io>
2012-08-24 15:17:49 +0200

    navbar clock: display localtime

--- web/js/jquery.clock.js
+++ web/js/jquery.clock.js
@@ -1,39 +1,28 @@
 /*
-Copyright 2012 Sven Reissmann <sven@0x80.io>
-
-This file is part of pyLogView. It is licensed under the terms of the 
-GNU General Public License version 3. See <http://www.gnu.org/licenses/>.
-*/
-
+ * Copyright 2012 Sven Reissmann <sven@0x80.io>
+ *
+ * This file is part of pyLogView. It is licensed under the terms of the
+ * GNU General Public License version 3. See <http://www.gnu.org/licenses/>.
+ */
 (function(jQuery) {
-	jQuery.fn.clock = function(options) {
-		var defaults = {
-			offset: '+0'
-		};
-		var _this = this;
-		var opts = jQuery.extend(defaults, options);
+	jQuery.fn.clock = function() {
+		var _this = this
 		
 		function refreshClock() {
-			var hours = jQuery.calcTime(opts.offset).getHours();
-			var minutes = jQuery.calcTime(opts.offset).getMinutes();
-			var seconds = jQuery.calcTime(opts.offset).getSeconds();
-			
+			d = new Date();
+			var hours = d.getHours();
+			var minutes = d.getMinutes();
+			var seconds = d.getSeconds();
+			var offset = -(d.getTimezoneOffset() / 60)
+
 			if (hours < 10) hours = '0' + hours;
 			if (minutes < 10) minutes = '0' + minutes;
 			if (seconds < 10) seconds = '0' + seconds;
+			if (offset > 0) offset = '+' + offset
 			
-			jQuery(_this).find(".hour").html(hours + ':');
-			jQuery(_this).find(".min").html(minutes + ':');
-			jQuery(_this).find(".sec").html(seconds);
+			jQuery(_this).find("#clock").html(hours + ':' + minutes + ':' + seconds + ' GMT' + offset);
 		}
 		refreshClock()
 		setInterval(refreshClock, 1000);
 	}
 })(jQuery);
-
-jQuery.calcTime = function(offset) {
-	d = new Date();
-	utc = d.getTime() + (d.getTimezoneOffset() * 60000);
-	nd = new Date(utc + (3600000 * offset));
-	return nd;
-};

738baaf8b5c65101c2430a9f3dd9b735faf00102
Sven Reissmann <sven@0x80.io>
2014-11-05 23:37:15 +0100

    Make wsgi parameters configurable. fixes #16

--- backend/src/logzen/__main__.py
+++ backend/src/logzen/__main__.py
@@ -17,18 +17,32 @@ You should have received a copy of the GNU General Public License
 along with LogZen. If not, see <http://www.gnu.org/licenses/>.
 """
 
-from require import require
+from require import require, extend
 
 from logzen.db.users import User
 from logzen.db.streams import Stream
 
 
+@extend('logzen.config:ConfigDecl')
+def BottleConfigDecl(config_decl):
+    with config_decl('wsgi') as section_decl:
+
+        # The ip address to listen on
+        section_decl('host',
+                     default='127.0.0.1')
+        # The tcp port to listen on
+        section_decl('port',
+                     default=8080)
+
+
 @require(app='logzen.web:App',
          session='logzen.db:Session',
-         users='logzen.db.users:Users')
+         users='logzen.db.users:Users',
+         config='logzen.config:Config')
 def main(app,
          session,
-         users):
+         users,
+         config):
     if session \
             .query(User) \
             .count() < 1:
@@ -45,7 +59,8 @@ def main(app,
 
     session.commit()
 
-    app.run()
+    app.run(host=config.wsgi.host,
+            port=config.wsgi.port)
 
 
 if __name__ == '__main__':
